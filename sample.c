e5fd322543efe9bb9d393794462846aef7e1db68
paveltrush <43778459+paveltrush@users.noreply.github.com>
2025-01-12 12:59:33 -0500

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,17 +1,34 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 #define BUFSIZE 256
-    
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
+    if (argc != 2) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    char cmd[BUFSIZE];
+
+    snprintf(cmd, BUFSIZE, "wc -c \"%s\"", argv[1]);
+
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) {
+        perror("popen failed");
+        return -1;
+    }
+
+    char result[BUFSIZE];
+    if (fgets(result, sizeof(result), pipe) != NULL) {
+        printf("%s", result);
+    } else {
+        fprintf(stderr, "Error: Could not retrieve the file size.\n");
+    }
+
+    pclose(pipe);
+    return 0;
 }
