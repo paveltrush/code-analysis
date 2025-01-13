99099270d9f45b9a532478b83259b753bd2e5ea5
paveltrush <43778459+paveltrush@users.noreply.github.com>
2025-01-13 13:36:24 -0500

    Update sample.c

--- sample.c
+++ sample.c
@@ -6,28 +6,22 @@
 
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) {
-        fprintf(stderr, "Please provide the address of a file as an input.\n");
-        return -1;
+  return read_file("/home/salazar/Documents/Contest/Ankitha enjoys finding.txt");
+}
+
+int read_file(const char *filename) {
+    FILE* ptr = fopen(filename, "r");
+
+    if (ptr == NULL) {
+        printf("File cannot be opened.\n");
+        return(-1);
     }
 
-    char* file_address = strdup(argv[1]);
+    char buff[BUFSIZE];
 
-    int limit_size = BUFSIZE - strlen("wc -c ");
-
-    if(strlen(file_address) > limit_size){
-        fprintf(stderr, "The file address value is too long.\n");
-        return -1;
-    }
-    
-    if (access(file_address, F_OK) != 0) {
-        printf("The file '%s' doesn't exists.\n", file_address);
+    while (fgets(buff, BUFSIZE, ptr) != NULL) {
+        printf("%s", buff);
     }
 
-    char *cmd[] = {"wc", "-c", file_address, NULL};
-    execvp(cmd[0], cmd);
-
-    perror("execvp failed");
-    return -1;
+    return 0;
 }
